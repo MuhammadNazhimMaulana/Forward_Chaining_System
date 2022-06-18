88b8d147f5d781b53a75557d054dde5a63eea44f
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-06-19 00:24:45 +0700

    Preparing Migration and seeder

---
+++ database/migrations/2022_06_18_172309_create_konsultasis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKonsultasisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('konsultasis', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('konsultasis');
+    }
+}
