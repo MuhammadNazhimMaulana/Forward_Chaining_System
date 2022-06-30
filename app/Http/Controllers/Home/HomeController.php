0f53530abfc097d9cbc2c6ae21e79efb48d4afa3
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-06-30 12:15:53 +0700

    Hotfix

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -35,7 +35,7 @@ class HomeController extends Controller
         }
 
         $data = [
-            "title" => "Home",
+            "title" => "Home User",
             "code" => min($list_gejala)
         ];
 
